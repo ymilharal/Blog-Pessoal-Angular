132fbf73710dd846fbb656e1f03878c64c018f44
Yasmim Marques <yasmim.msantos@outlook.com>
2022-02-02 01:35:19 -0300

    Pesquisas implementadas

--- blogCantinho/src/app/service/tema.service.ts
+++ blogCantinho/src/app/service/tema.service.ts
@@ -24,6 +24,10 @@ export class TemaService {
     return this.http.get<Tema>(`https://cantinhocafe.herokuapp.com/tema/${id}`, this.token)
   }
 
+  getByNomeTema(nome: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`https://cantinhocafe.herokuapp.com/tema/nome/${nome}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://cantinhocafe.herokuapp.com/tema', tema, this.token)
 
